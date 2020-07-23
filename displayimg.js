f4b22ca6d4d2ff22088a7187c4b9e0a196aeef72
Lucy Wan <lucy.w@berkeley.edu>
2020-07-23 11:11:53 -0700

    Eraewr

--- displayimg.js
+++ displayimg.js
@@ -1,12 +1,11 @@
-var folder = "img/beach/";
+var names = ["IMG_6282.jpg","IMG_6510.jpg","IMG_6669_2.jpg","IMG_6679_2.jpg","IMG_6373.jpg","IMG_6512.jpg","IMG_6672_2.jpg","IMG_6681_2.jpg","IMG_6507.jpg","IMG_6513.jpg","IMG_6676_2.jpg","IMG_6685_2.jpg","IMG_6508.jpg","IMG_6515.jpg","IMG_6678_2.jpg"];
+var folder = "img/beach/"
 
-$.ajax({
-    url : folder,
-    success: function (data) {
-        $(data).find("a").attr("href", function (i, val) {
-            if( val.match(/\.(jpe?g|png|gif)$/) ) { 
-                $("body").append( "<img src='"+ folder + val +"'>" );
-            } 
-        });
-    }
-});
+
+var i;
+for (i = 0; i < names.length; i++)  
+{ 
+
+    $("body").append( "<img src='"+ folder + names[i] +"' class = 'foodpic'>" );
+
+}
---
+++ img/displaypics.js
@@ -0,0 +1,59 @@
+function beaches(){
+var names = ["IMG_6282.jpg","IMG_6510.jpg","IMG_6669_2.jpg","IMG_6679_2.jpg","IMG_6373.jpg","IMG_6512.jpg","IMG_6672_2.jpg","IMG_6681_2.jpg","IMG_6507.jpg","IMG_6513.jpg","IMG_6676_2.jpg","IMG_6685_2.jpg","IMG_6508.jpg","IMG_6515.jpg","IMG_6678_2.jpg"];
+var folder = "img/beach/"
+
+
+var i; 
+  
+for (i = 0; i < names.length; i++)  
+{ 
+
+    $("body").append( "<img src='"+ folder + names[i] +"' class = 'foodpic'>" );
+
+} 
+}
+
+function berkeley(){
+var names = ["IMG_5962.jpg","IMG_6034.jpg"];
+var folder = "img/berkeley/"
+
+
+var i; 
+  
+for (i = 0; i < names.length; i++)  
+{ 
+
+    $("body").append( "<img src='"+ folder + names[i] +"' class = 'foodpic'>" );
+
+} 
+}
+
+function wildlife(){
+var names = ["IMG_6218.jpg"];
+var folder = "img/wildlife/"
+
+
+var i; 
+  
+for (i = 0; i < names.length; i++)  
+{ 
+
+    $("body").append( "<img src='"+ folder + names[i] +"' class = 'foodpic'>" );
+
+} 
+}
+
+function cats(){
+var names = ["60678782839__262FAB31-56EC-414C-B59A-407D063807A4.JPG"];
+var folder = "img/cats/"
+
+
+var i; 
+  
+for (i = 0; i < names.length; i++)  
+{ 
+
+    $("body").append( "<img src='"+ folder + names[i] +"' class = 'foodpic'>" );
+
+} 
+}
