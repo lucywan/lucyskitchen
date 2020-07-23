d7bd13d69e5e91681f59e08671cad0af3feea97c
Lucy Wan <lucy.w@berkeley.edu>
2020-07-22 22:47:38 -0700

    Erews

--- photography.php
+++ photography.php
@@ -6,9 +6,9 @@
 	</head>
 <body class  = "b">
 	<div class = "nav">
-<a href = "index.html">Recipies</a>
-<a href = "photography.html">Photography</a>
-<a href = "gardening.html">Garden</a>
+<a href = "https://lucywan.github.io/lucyskitchen/index.html">Recipies</a>
+<a href = "photography.php">Photography</a>
+<a href = "https://lucywan.github.io/lucyskitchen/gardening.html">Garden</a>
 </div>
 <h1 class = "header" align = "center">Welcome to Lucy's Kitchen!</h1>
 
@@ -16,6 +16,18 @@
 <img src = "img/beach/IMG_6508.jpg" class = "foodpic">
 <img src = "img/beach/IMG_6507.jpg" class = "foodpic">
 
+<ul>
+<!-- <?php
+$dirname = "img/beach/";
+$images = glob($dirname."*.jpg");
+
+foreach($images as $image) {
+    echo '<img src="'.$image.'" /><br />';
+}
+
+}
+?> -->
+</ul>
 
 </body>
 </html>
